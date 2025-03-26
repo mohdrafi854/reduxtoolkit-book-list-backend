3a26e183cd0cf60e515b8307712bd7527744fc40
mohdrafi854 <mohd.rafi854@gmail.com>
2025-03-26 17:55:06 +0530

    backend push

---
+++ db/db.connect.js
@@ -0,0 +1,16 @@
+const mongoose = require("mongoose");
+require("dotenv").config();
+const mongoURI = process.env.MONGODB_URI;
+
+const initializeDatabase = async () => {
+  try {
+    const connection = await mongoose.connect(mongoURI);
+    if (connection) {
+      console.log("Connected Successfully");
+    }
+  } catch (error) {
+    console.log("Connection Failed", error);
+  }
+};
+
+module.exports = { initializeDatabase };
---
+++ models/books.model.js
@@ -0,0 +1,10 @@
+const mongoose = require("mongoose");
+
+const bookSchema = new mongoose.Schema({
+    bookName : String,
+    author : String,
+    genre : String,
+})
+
+const Books = mongoose.model("Books", bookSchema);
+module.exports = {Books}
